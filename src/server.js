522c6aa3dbc78c7a21ec491051e4e2ca097014b1
Wesley Paternezi <wesley.paternezi@bandtec.com.br>
2022-05-27 18:43:29 -0300

    serverHttp

--- src/server.js
+++ src/server.js
@@ -1,4 +1,22 @@
-const app = require('./app');
+require('dotenv').config({
+    path: process.env.NODE_ENV == "test" ? ".env.test" : ".env"
+});
+let express = require("express");
+let http = require("http");
+let cors = require('cors');
+
+let router = require('./routes');
+
+const server = express();
+
+server.use(cors());
+
+const serverHttp = http.createServer(server);
+
+server.use(express.json());
+
+server.use(router);
+
 const PORT = process.env.PORT || 8081
 
-app.listen(PORT, () => console.log(`is running on port ${PORT}`));
+serverHttp.listen(PORT, () => console.log(`is running on port ${PORT}`));
